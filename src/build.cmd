036b55b60b9a95c39d5d8dad6d66430f2f706213
Ian Joseph Thompson <ian.joseph.thompson@gmail.com>
2016-02-19 21:18:20 -0800

    Build infrastructure and initial documentation.

---
+++ src/build.cmd
@@ -0,0 +1,31 @@
+:: *************************    Begin License Block    *************************
+:: TrekLog by Ian Joseph Thompson (Copyright 2016, CC BY-NC-SA 4.0)
+::     TrekLog, the contents of this file, and other source files are licensed
+::     under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+::     International License (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+::     as documented in the file LICENSE.txt.
+:: 
+:: You are free to:
+::     Share - copy and redistribute the material in any medium or format
+::     Adapt - remix, transform, and build upon the material
+::     Independently - The licensor cannot revoke these freedoms as long as you
+::         follow the license terms.
+:: 
+:: Under the following terms:
+::     Attribution - You must give appropriate credit, provide a link to the
+::         license, and indicate if changes were made. You may do so in any
+::         reasonable manner, but not in any way that suggests the licensor
+::         endorses you or your use.
+::     NonCommercial - You may not use the material for commercial purposes.
+::     ShareAlike - If you remix, transform, or build upon the material, you must
+::         distribute your contributions under the same license as the original.
+::     No additional restrictions - You may not apply legal terms or technological
+::         measures that legally restrict others from doing anything the license
+::         permits.
+:: 
+:: Removing this information violates the terms of the license:
+::     Source: TrekLog (https://github.com/ianjosephthompson/TrekLog)
+::     By:     Ian Joseph Thompson (ian.joseph.thompson@gmail.com)
+:: **************************    End License Block    **************************
+
+pyinstaller --workpath ../work --specpath /work --distpath ../build --onefile --clean TrekLog/TrekLog.py
